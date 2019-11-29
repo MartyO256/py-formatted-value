744aa734903b5daf72c6ce14e7517e483290391e
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2019-11-28 20:47:49 -0500

    Implement formatting with power of 10 suffix

--- fvalue/fvalue.py
+++ fvalue/fvalue.py
@@ -1,9 +1,11 @@
 from decimal import Decimal, ROUND_HALF_EVEN, ROUND_05UP, ROUND_CEILING, \
     ROUND_DOWN, ROUND_FLOOR, ROUND_HALF_DOWN, ROUND_HALF_UP, ROUND_UP
-from enum import Enum
-from typing import Tuple, Union
+from enum import Enum, unique
+from math import floor
+from typing import Tuple, Union, Callable
 
 
+@unique
 class RoundingOption(Enum):
     ROUND_HALF_EVEN = ROUND_HALF_EVEN
     ROUND_05UP = ROUND_05UP
@@ -17,11 +19,11 @@ class RoundingOption(Enum):
 
 class FormattedValue:
     def __init__(
-        self,
-        value: Union[int, float, Decimal],
-        error: Union[int, float, Decimal] = 0,
-        error_significant_figures: int = 1,
-        rounding: RoundingOption = RoundingOption.ROUND_HALF_EVEN,
+            self,
+            value: Union[int, float, Decimal],
+            error: Union[int, float, Decimal] = 0,
+            error_significant_figures: int = 1,
+            rounding: RoundingOption = RoundingOption.ROUND_HALF_EVEN,
     ):
         self.value = value
         self.error = error
@@ -60,7 +62,7 @@ class FormattedValue:
 
     @rounding.setter
     def rounding(self, rounding: RoundingOption) -> None:
-        if rounding not in RoundingOption:
+        if RoundingOption(rounding) not in RoundingOption:
             raise ValueError(f"Unsupported rounding option {rounding}.")
         self.__rounding = rounding
 
@@ -69,7 +71,7 @@ class FormattedValue:
             multiplier: Union[None, int, float]
     ) -> Decimal:
         error = Decimal(self.error)
-        error = error if multiplier is None else error * Decimal(multiplier)
+        error = error * Decimal(multiplier) if multiplier else error
         significant_error_format = f"%#.{self.error_significant_figures}g"
         return error.quantize(
             Decimal(significant_error_format % error),
@@ -82,7 +84,7 @@ class FormattedValue:
             multiplier: Union[None, int, float]
     ) -> Decimal:
         value = Decimal(self.value)
-        value = value if multiplier is None else value * Decimal(multiplier)
+        value = value * Decimal(multiplier) if multiplier else value
         return value.quantize(
             rounded_error,
             rounding=self.rounding.value
@@ -95,23 +97,47 @@ class FormattedValue:
     def rounded_data(
             self,
             multiplier: Union[None, int, float] = None,
-    ) -> Tuple[Decimal, Decimal]:
+    ) -> Tuple[Decimal, Decimal, int]:
         error = self._rounded_error(multiplier)
         value = self._rounded_value(error, multiplier)
-        return value, error
-        
+        error_significant_figures = self.error_significant_figures
+        exponent = floor(error.log10())
+        exponent = 0 if exponent < error_significant_figures \
+            else exponent - error_significant_figures + 1
+        return value.scaleb(-exponent), error.scaleb(-exponent), exponent
+
+    SIUNITX_TEMPLATE = r"\SI{{{0} \pm {1} e{2}}}{{{3}}}"
+    SIUNITX_NUM_TEMPLATE = r"\num{{{0} \pm {1} e{2}}}"
+
     def formatted(
             self,
-            template: str = r"\SI{{{0} \pm {1}}}{{{2}}}",
+            template: Union[None, str, Callable[[str, str, str, str], str]],
             multiplier: Union[None, int, float] = None,
             units: str = "",
     ) -> str:
-        rounded_value, rounded_error = self.rounded_data(multiplier)
-        return template.format(
-            "{:f}".format(rounded_value),
-            "{:f}".format(rounded_error),
-            units,
-        )
+        template = template if template else FormattedValue.SIUNITX_TEMPLATE
+        rounded_value, rounded_error, exponent = self.rounded_data(multiplier)
+        value = "{:f}".format(rounded_value)
+        error = "{:f}".format(rounded_error)
+        exponent = "{:d}".format(exponent)
+        if type(template) is str:
+            return template.format(value, error, exponent, units)
+        else:
+            return template(value, error, exponent, units)
+
+    @staticmethod
+    def _natural_format(value: str, error: str, exponent: str, units: str):
+        if exponent == "0":
+            return f"({value} ± {error}) {units}" if units \
+                else f"{value} ± {error}"
+        elif exponent == "1":
+            return f"({value} ± {error}) x 10 {units}" if units \
+                else f"({value} ± {error}) x 10"
+        else:
+            return f"({value} ± {error}) x 10^{exponent} {units}" if units \
+                else f"({value} ± {error}) x 10^{exponent}"
+
+    NATURAL_TEMPLATE = _natural_format
 
     def __str__(self) -> str:
-        return self.formatted("{0} ± {1}")
+        return self.formatted(FormattedValue.NATURAL_TEMPLATE)
